331550375f7e44b17909370777660c9075618644
namazovanton <namazovanton@gmail.com>
2023-02-19 10:43:04 +0300

    SQLALCHEMY_DATABASE_URI is fixed

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config(object):
     DEBUG = True
     SECRET_HERE = '249y823r9v8238r9u'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://flask_app:Darina1998@localhost/flask_app'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ITEMS_PER_PAGE = 12
     MAX_PAGE = 100
